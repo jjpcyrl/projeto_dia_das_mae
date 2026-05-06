2f1cfeb4136652e6663892f68b163aeed60076be
jjpcyrl <joaopedrodossantosbispo310@gmail.com>
2026-05-06 09:15:08 -0300

    fix: remover rota assinar

--- project/backend/routes/surpresa.js
+++ project/backend/routes/surpresa.js
@@ -1,9 +1,6 @@
-const express = require("express");
+﻿const express = require("express");
 const router  = express.Router();
 const ctrl    = require("../controllers/surpresaController");
-
-router.get("/assinar", ctrl.assinar);
 router.post("/", express.json(), ctrl.criar);
 router.get("/:id", ctrl.buscar);
-
 module.exports = router;
