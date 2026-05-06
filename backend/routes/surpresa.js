4bfca2340e73143dd80912ac5603fd0a02d8da63
jjpcyrl <joaopedrodossantosbispo310@gmail.com>
2026-05-06 09:30:01 -0300

    update

--- backend/routes/surpresa.js
+++ backend/routes/surpresa.js
@@ -2,7 +2,6 @@ const express = require("express");
 const router  = express.Router();
 const ctrl    = require("../controllers/surpresaController");
 
-router.get("/assinar", ctrl.assinar);
 router.post("/", express.json(), ctrl.criar);
 router.get("/:id", ctrl.buscar);
 
